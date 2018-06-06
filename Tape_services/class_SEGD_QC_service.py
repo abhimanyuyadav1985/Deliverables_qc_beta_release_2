a9deff325a2d05cf5d108a99677f8bf75d401c55
Abhimanyu Yadav <yadav.manyu@gmail.com>
2018-06-06 13:48:07 +0400

    now handles the creation of SEGD QC command for all types of SEGD recording modes

--- Tape_services/class_SEGD_QC_service.py
+++ Tape_services/class_SEGD_QC_service.py
@@ -5,6 +5,7 @@ import posixpath
 from dug_ops.DUG_ops import append_register_entry
 from database_engine.DB_ops import get_all_raw_seq_info_objects
 from configuration import get_segd_qc_script
+from database_engine.DB_ops import get_segd_qc_path
 
 import logging
 from app_log import  stream_formatter
@@ -25,14 +26,30 @@ class SEGD_QC_service(object):
         self.segd_qc_script = get_segd_qc_script()
         logger.info("SEGD QC Script: " + self.segd_qc_script)
 
+    def setup_execution(self,reel_no,file_list,deliverable,drive,set, split_line):
+        self.reel_no = reel_no
+        self.deliverable = deliverable
+        self.dst = drive
+        self.file_list = file_list
+        self.set = set
+        self.split_line = split_line
+        self.set_SEGD_path()
+        self.set_logfile()
+        self.run()
+
+
     def run(self):
-        run_cmd = str("nohup " + self.segd_qc_script + " tape=/dev/" + str(self.parent.tape_drive)+ " disk=" + str(self.SEGD_path) + " log=" + str(self.logfile) + " firstdisk=" + str(self.min_ffid) + " lastdisk=" + str(self.max_ffid) + ' > /dev/null 2>&1 &')
+        if self.split_line == True:
+            run_cmd = str("nohup " + self.segd_qc_script + " tape=/dev/" + str(self.dst)+ " disk=" + self.SEGD_path_string + " log=" + str(self.logfile) + " firstdisk=" + str(self.min_ffid) + " lastdisk=" + str(self.max_ffid) + ' > /dev/null 2>&1 &')
+        else:
+            run_cmd = str("nohup " + self.segd_qc_script + " tape=/dev/" + str(self.dst) + " disk=" +
+                self.SEGD_path_string + " log=" + str(self.logfile) + ' > /dev/null 2>&1 &')
         if use_mode == 'Demo':
             logger.warning("Running in DEMO mode command will only be printed not executed ....")
         elif use_mode == 'Production':
             logger.info("Now sending to DUG buffer: " + run_cmd)
             self.check_for_previous_run()
-            segd_qc_register_obj = [run_cmd , self.parent.tape_drive, self.logfile , 'segd_qc']
+            segd_qc_register_obj = [run_cmd , self.dst, self.logfile , 'segd_qc']
             append_register_entry(self.parent.DUG_connection_obj,segd_qc_register_obj)
 
     def check_for_previous_run(self):
@@ -46,13 +63,17 @@ class SEGD_QC_service(object):
             self.parent.db_connection_obj.sess.commit()
 
 
-
-    def set_SEGD_path(self,Seq_dir):
-        self.SEGD_path = posixpath.join(self.parent.dir_service.data_dir_path_dict['segd.segd'],Seq_dir)
-        logger.info("The SEGD data directory set:: " + self.SEGD_path)
+    def set_SEGD_path(self):
+        self.SEGD_path_string = ''
+        for i in range(0,len(self.file_list)):
+            self.SEGD_path_string = self.SEGD_path_string + ' ' + posixpath.join(self.parent.dir_service.data_dir_path_dict['segd.segd'],self.file_list[i])
+        logger.info("The SEGD data directory set")
 
     def set_logfile(self):
-        self.logfile = self.parent.file_service.set_segd_log_file_path()
+        search_obj = get_segd_qc_path(self.parent.db_connection_obj, self.parent.deliverable.id, self.set)
+        dir_path = search_obj.path
+        file_name = self.reel_no + "--set" + self.parent.set_no + '.log'
+        self.logfile= posixpath.join(dir_path, file_name)
         logger.info("The log file set :: " + self.logfile)
 
     def get_list_of_available_segd_seq(self):
@@ -80,13 +101,12 @@ class SEGD_QC_service(object):
         return return_list
 
 
-    def get_list_of_applicable_segd_tapes(self):
-        tape_list = get_list_of_applicable_SEGD_tapes(self.parent.db_connection_obj,self.parent.seq_name)
+    def get_list_of_applicable_segd_tapes(self, file_list):
+        tape_list = get_list_of_applicable_SEGD_tapes(self.parent.db_connection_obj,file_list)
         return tape_list
 
     def set_min_max_ffid(self):
-        (self.min_ffid,self.max_ffid) = get_min_max_ffid_tuple_for_tape(self.parent.db_connection_obj,self.parent.tape_name)
-
+        (self.min_ffid,self.max_ffid) = get_min_max_ffid_tuple_for_tape(self.parent.db_connection_obj,self.parent.reel_no)
 
 if __name__ == '__main__':
     test = SEGD_QC_service()
